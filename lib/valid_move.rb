08cf729df1f3b0c4c8e927cdb56360f9a7324475
Farzad Daud <daud.farzad1@gmail.com>
2020-01-13 21:24:54 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,21 +1,25 @@
-def valid_move?(board, position)
-	spot = position.to_i - 1
-	if !position_taken?(board, spot) && spot.between?(0,8)
-		true
+def valid_move?(board, index)
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
+    else
+      return true
+    end
+  end
 
-	else
-	false
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
 
-end
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end
 
-end
-
-		def position_taken?(board, position)
-		  if board[position] == " " || board[position] == "" || board[position] == nil
-		  	false
-			
-true
-
-end
 
 end
